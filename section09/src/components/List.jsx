66a1f610428755a6fc5967f7fab409044ec98276
dlacowns21 <dlacowns21@naver.com>
2025-01-24 17:33:37 +0900

    [FEAT] study until 9.2

---
+++ section09/src/components/List.jsx
@@ -0,0 +1,44 @@
+import "./List.css";
+import TodoItem from "./TodoItem";
+import { useState } from "react";
+
+const List = ({ todos, onUpdate, onDelete }) => {
+  const [search, setSerach] = useState("");
+  const onChangeSearch = (e) => {
+    setSerach(e.target.value);
+  };
+  const getFilteredData = () => {
+    if (search === "") {
+      return todos;
+    }
+    return todos.filter((todo) =>
+      todo.content.toLowerCase().includes(search.toLowerCase())
+    );
+  };
+
+  const filteredTodos = getFilteredData();
+  return (
+    <div className="List">
+      <h4>Todo List 😊</h4>
+      <input
+        value={search}
+        onChange={onChangeSearch}
+        placeholder="검색어를 입력해주세요"
+      />
+      <div className="todos_wrapper">
+        {filteredTodos.map((todo) => {
+          return (
+            <TodoItem
+              key={todo.id}
+              {...todo}
+              onUpdate={onUpdate}
+              onDelete={onDelete}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default List;
